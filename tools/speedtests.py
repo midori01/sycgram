a494f3fde6f7ff470056df3bb78acbf007246ee4
ミドリ <midori01@users.noreply.github.com>
2023-04-03 18:11:56 +0800

    Update speedtests.py

--- tools/speedtests.py
+++ tools/speedtests.py
@@ -48,13 +48,13 @@ class Speedtester:
             return f"⚠️ Speedtest Error\n```{res.get('error')}```", ''
         else:
             text = "**Speedtest**\n" \
-                f"测速点: {self.get_server()}\n" \
-                f"服务商: {self.get_sponsor()}\n" \
-                f"上传速度: {self.get_speed('upload')}\n" \
-                f"下载速度: {self.get_speed('download')}\n" \
-                f"抖动: {self.get_ping('jitter')}\n" \
+                f"测速点: {self.get_sponsor()}\n" \
+                f"位置: {self.get_server()}\n" \
+                f"下载: {self.get_speed('download')}\n" \
+                f"上传: {self.get_speed('upload')}\n" \
                 f"延迟: {self.get_ping('latency')}\n" \
-                f"测速时间: {self.get_time()}"
+                f"抖动: {self.get_ping('jitter')}\n" \
+                f"时间: {self.get_time()}"
             return text, f"{self.__output.get('result').get('url')}.png"
 
     async def list_servers_ids(self, cmd: str) -> str:
